2b725772e185bd2932aa94b0dad4606c78253540
Watcharapong.K <147409902+Voltran1c@users.noreply.github.com>
2024-05-14 14:17:08 +0700

    Js function1 commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// Exercise 1.1 Create function following below
+function addNum(a, b) {
+  return a + b;
+}
+console.log(addNum(5, 6));
+
+
+function subtract(a, b) {
+  return a - b;
+}
+console.log(subtract(7, 3));
+
+
+function multiply(a, b) {
+  return a * b;
+}
+console.log(multiply(5, 100));
+
+
+function divide(a, b) {
+  return a / b;
+}
+console.log(divide(99, 11));
+
+// Exercise 1.2 Create Function Inform
+function inform(firstName, Location, Hobby){
+    console.log("Hi, my name is " + firstName + " I live in " + Location + " and enjoy "+ Hobby);
+}
+inform("Watcharapong", "Chiangmai", "Watch streamming");
